42e6e28b65b63bc565984ae92e58f692d0a6305d
Madhur Prakash <madhurprakash46@gmail.com>
2020-08-07 02:35:03 +0530

    6

--- codeforces/settings.py
+++ codeforces/settings.py
@@ -123,8 +123,8 @@ STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
